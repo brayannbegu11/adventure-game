6d88d786e3a73dba50a1ee88b2893a28d7f74ecd
Brayann Benavides <brayannbegu11@gmail.com>
2024-11-13 13:01:04 -0500

    Adding struct data history, and saving that in a hashmap with the key tag

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,30 @@
 use csv::{Reader, ReaderBuilder, StringRecord};
+use std::collections::HashMap;
 use std::fs;
 
 const FILENAME: &str = "history.csv";
 
+// TYPE, TAG, TEXT, LIFE
+struct DataHistory {
+    data_type: String,
+    tag: String,
+    text: String,
+    life: i32,
+}
+
+impl DataHistory {
+    fn new(row: StringRecord) -> DataHistory {
+        return DataHistory {
+            data_type: row.get(0).unwrap().trim().to_string(),
+            tag: row.get(1).unwrap().trim().to_string(),
+            text: row.get(2).unwrap().trim().to_string(),
+            life: row.get(3).unwrap().trim().parse().unwrap_or(0),
+        };
+    }
+}
+
 fn main() {
+    let mut data_history: HashMap<String, DataHistory> = HashMap::new();
     let content = fs::read_to_string(FILENAME).unwrap();
 
     // println!("{:?}", content);
@@ -12,6 +33,15 @@ fn main() {
         .delimiter(b';')
         .from_reader(content.as_bytes());
     for result in rdr.records() {
-        println!("Texto: {}", result.unwrap().get(2).unwrap().trim());
+        let result = result.unwrap();
+
+        let data = DataHistory::new(result);
+
+        data_history.insert(data.tag.clone(), data);
     }
+
+    let mut dict: HashMap<String, String> = HashMap::new();
+    dict.insert("Apple".to_string(), "Red color fruit".to_string());
+
+    println!("Description: {}", dict["Apple"]);
 }
