ab3a0d3784c1195de24f8ccb6b373357f082f08a
Brayann Benavides <brayannbegu11@gmail.com>
2024-11-13 12:28:35 -0500

    Reading the csv file of the story

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use csv::{Reader, ReaderBuilder, StringRecord};
+use std::fs;
+
+const FILENAME: &str = "history.csv";
+
 fn main() {
-    println!("Hello, world!");
+    let content = fs::read_to_string(FILENAME).unwrap();
+
+    // println!("{:?}", content);
+
+    let mut rdr = ReaderBuilder::new()
+        .delimiter(b';')
+        .from_reader(content.as_bytes());
+    for result in rdr.records() {
+        println!("Texto: {}", result.unwrap().get(2).unwrap().trim());
+    }
 }
